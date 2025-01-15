8460d40851f252ee2bc1db10f0e0d7ab752a6c00
Digvijay Pandit <145995817+digvijaypandit@users.noreply.github.com>
2025-01-15 14:30:42 +0530

    error fixed

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,9 +1,13 @@
 import {asyncHandler} from '../utils/asyncHandler.js';
+import {ApiError} from '../utils/apiError.js'
 
 const registerUser = asyncHandler(async (req,res) => {
-    res.status(200).json({
-        message:"ok"
-    })
+    const {fullName,email,username,password} = req.body
+    console.log(fullName, email, username, password)
+
+    if (fullName === ""){
+        throw new ApiError(400,"fullname is required");
+    }
 })
 
 export {registerUser}
