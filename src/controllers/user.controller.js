0db7d4b9f45b29def9bd7f722edb056d5e05d69d
Digvijay Pandit <145995817+digvijaypandit@users.noreply.github.com>
2025-01-17 19:29:22 +0530

    fix: update logoutUser to unset refreshToken instead of setting it to undefined

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -125,8 +125,8 @@ const logoutUser = asyncHandler(async(req, res) => {
     await User.findByIdAndUpdate(
         req.user._id,
         {
-            $set: {
-                refreshToken: undefined
+            $unset: {
+                refreshToken: 1
             }
         },
         {
@@ -291,8 +291,6 @@ const updateUserCoverImage = asyncHandler(async(req,res)=>{
     )
 
 })
-
-
 
 const getUserChannelProfile = asyncHandler(async(req,res) => {
     const {username} = req.params
